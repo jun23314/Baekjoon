6c22475303cd4d6c5aaae92814bbf2421e5e479b
이준희 <116951160+jun23314@users.noreply.github.com>
2025-10-16 23:20:27 +0900

    [Gold V] Title: 두 용액, Time: 60 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+#define INF 2000000001
+
+using namespace std;
+
+int n;
+pair<int, int> p;
+int ans = 0;
+int water[100001];
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; ++i) cin >> water[i];
+	sort(water, water + n);
+
+	ans = INF;
+	int l = 0, r = n - 1;
+	while (l < r) {
+		int tmp = water[l] + water[r];
+
+		if (abs(tmp) < ans) { // 유효한 값 도출
+			ans = abs(tmp);
+			p = make_pair(water[l], water[r]);
+			if (ans == 0) break;
+		}
+
+		if (tmp < 0) l++;
+		else r--;
+	}
+
+	cout << p.first << " " << p.second;
+}
