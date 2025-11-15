5225fea3aad73650ab275ff1cadaa68de8591edc
이준희 <116951160+jun23314@users.noreply.github.com>
2025-11-16 02:51:32 +0900

    [Gold V] Title: 용액, Time: 244 ms, Memory: 31768 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,12 +1,10 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     private static int N;
-    private static int[] liquid;
+    private static int[] liquid = new int[100001];
 
     void solve() {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
